faeaa8c54ca8ee1331e96f6dc7a166af53e954e9
brahyun <2139477@sd61learn.ca>
2024-10-17 14:19:32 -0700

    update

--- LairdCade Gambling/titlescreen.gd
+++ LairdCade Gambling/titlescreen.gd
@@ -13,3 +13,5 @@ func _process(delta: float) -> void:
 
 func _on_playbutton_button_down() -> void:
 	get_tree().change_scene_to_file("res://homescreen.tscn")
+func _on_how_2_play_button_down() -> void:
+	pass # Replace with function body.
