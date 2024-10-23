41d98feb379ccc775a94e78bb4022ca64d77ece2
brahyun <2139477@sd61learn.ca>
2024-10-23 13:14:54 -0700

    update

--- LairdCade Gambling/how_2_play.gd
+++ LairdCade Gambling/how_2_play.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Button
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,3 +9,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_button_down() -> void:
+	$buttonclick.play()
+	
+func _on_buttonclick_finished() -> void:
+	get_tree().change_scene_to_file("res://info.tscn")
