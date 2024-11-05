85a9094eded339d407a1630a9beb10d5f63945dc
Eli <1146908@sd61learn.ca>
2024-11-05 14:07:46 -0800

    working plinko select screen

--- LairdCade Gambling/plinko.gd
+++ LairdCade Gambling/plinko.gd
@@ -14,7 +14,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	$scoretext.text = "Score: " + str(score)
-	print(dropable)
+	
 
 
 
--- LairdCade Gambling/plinkospawner.gd
+++ LairdCade Gambling/plinkospawner.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-var chip = preload("res://pokerchip.tscn")
+
+@onready var chip = load(Global.current_plinko_select)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
