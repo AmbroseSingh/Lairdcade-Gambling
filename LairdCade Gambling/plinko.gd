0a1bad40bb3c5205865950678b722ea02607fce1
Eli <1146908@sd61learn.ca>
2024-11-07 14:22:13 -0800

    finished plinko

--- LairdCade Gambling/plinko.gd
+++ LairdCade Gambling/plinko.gd
@@ -2,7 +2,6 @@ extends Node2D
 var r = randi_range(0,255)
 var g = randi_range(0,255)
 var b = randi_range(0,255)
-var score = 0
 var dropable = true
 
 
@@ -13,10 +12,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$scoretext.text = "Score: " + str(score)
+	$scoretext.text = "Score: " + str(Global.current_score)
 	
-
-
 
 
 #func _on_timer_timeout() -> void:
@@ -25,22 +22,63 @@ func _process(delta: float) -> void:
 
 
 func _on_green_body_entered(body: Node2D) -> void:
-
-		score = score + 200
+		compare_current_selection(200)
 		dropable = true
 		
 
 func _on_yellow_body_entered(body: Node2D) -> void:
 	
-		score = score + 10
+		compare_current_selection(10)
 		dropable = true
 		
 
 func _on_red_body_entered(body: Node2D) -> void:
 	
-		score = score - 10
+		compare_current_selection(-10)
 		dropable = true
 		
+
+func compare_current_selection(scoreamount):
+	match Global.current_plinko_select:
+		"res://marvel_chip.tscn":
+			Global.MarvelToken+= scoreamount
+			Global.current_score.text = Global.MarvelToken.text
+		"res://fortnite_chip.tscn":
+			Global.FortniteToken+= scoreamount
+			Global.current_score = Global.FortniteToken
+		"res://breaking_bad_chip.tscn":
+			Global.BreakingBadToken+= scoreamount
+			Global.current_score.text = Global.BreakingBadToken
+		"res://family_guy_chip.tscn":
+			Global.FamilyGuyToken+= scoreamount
+			Global.current_score.text = Global.FamilyGuyToken
+		"res://spongebob_chip.tscn":
+			Global.SpongebobToken+= scoreamount
+			Global.current_score.text = Global.SpongebobToken
+		"res://the_boys_chip.tscn":
+			Global.FortniteToken+= scoreamount
+			Global.current_score.text = Global.TheBoysToken
+		"res://misc_chip.tscn":
+			Global.MiscToken+= scoreamount
+			Global.current_score.text = Global.MiscToken
+
+"""func compare_current_score():
+	match Global.current_plinko_select:
+		"res://marvel_chip.tscn":
+			Global.current_score.text = Global.MarvelToken.text
+		"res://fortnite_chip.tscn":
+			Global.current_score.text = Global.FortniteToken.text
+		"res://breaking_bad_chip.tscn":
+			Global.current_score.text = Global.BreakingBadToken
+		"res://family_guy_chip.tscn":
+			Global.current_score.text = Global.FamilyGuyToken
+		"res://spongebob_chip.tscn":
+			Global.current_score.text = Global.SpongebobToken
+		"res://the_boys_chip.tscn":
+			Global.current_score.text = Global.TheBoysToken
+		"res://misc_chip.tscn":
+			Global.current_score.text = Global.MiscToken"""
+
 
 
 func _on_backbutton_p_pressed() -> void:
