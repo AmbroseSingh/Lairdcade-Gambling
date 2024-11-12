da2221f6f463e252310df66ba8303c184da2229b
Ambrose Otto Singh <Ambrose@Hansis-MacBook-Pro.local>
2024-11-12 10:36:08 -0800

    fix

--- LairdCade Gambling/fortnite_logo.gd
+++ LairdCade Gambling/fortnite_logo.gd
@@ -8,4 +8,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$FNScore.text = "Tokens: " + str(Global.FortniteToken)
+	$FNScore.text = str(Global.FortniteToken)
